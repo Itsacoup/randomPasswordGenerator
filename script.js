2e94ca3756c1b1b8b5757a58d99c5d6a35f5ff38
Matt Baber <mbaber1142@gmail.com>
2020-09-12 14:32:43 -0400

    restructed and debugged validation

--- script.js
+++ script.js
@@ -93,27 +93,85 @@ var numberChar = [
   '8',
   '9'
 ];
+var passOption = []
 
+//these event listerners call the beginValidation function and remove the readonly installed in the index when button is clicked
+generateBtn.addEventListener("click", beginValidation);
+generateBtn.addEventListener("click", document.getElementById('password').removeAttribute('readonly'));
 
+// this function generates the password when validation is met
+function generatePassword(passLength) {
+  var password = "";
 
+  //this loop runs through the array we created above, conisting of place holders that denote which chartype array we will perform random math on
+  for (var i = 0; i < passLength; i++) {
+    var x = passOption[Math.floor(Math.random() * passOption.length)];
+    //these are 4 seperate functions, one for each character type, as they are fired they return their value to concatenate to password
+    function lowerCased() {
+      var y = lowerCasedChar[Math.floor(Math.random() * lowerCasedChar.length)];
+      return password += y;
+    }
 
-// this function writes the password when generate button is clicked
-function writePassword() {
-  // validation
+    function upperCased() {
+      var y = upperCasedChar[Math.floor(Math.random() * upperCasedChar.length)];
+      return password += y;
+    }
+
+    function number() {
+      var y = numberChar[Math.floor(Math.random() * numberChar.length)];
+      return password += y;
+    }
+
+    function special() {
+      var y = specialChar[Math.floor(Math.random() * specialChar.length)];
+      return password += y;
+    }
+    //this set of conidtions is what the loop is acting on, which ever string in the array is iterated, it will compare to match, and fire the appropriate of the 4 functions
+    if (x == "up") {
+      upperCased();
+      console.log(password)
+    }
+
+    if (x == "low") {
+      lowerCased();
+      console.log(password)
+    }
+
+    if (x == "num") {
+      number();
+      console.log(password)
+    }
+
+    if (x == "spec") {
+      special();
+      console.log(password)
+    }
+  }
+  writePassword(password);
+}
+
+//this function takes the return of generatePassword and writes it to the page
+function writePassword(password) {
+  passOption = []
+  document.getElementById("password").innerHTML = (password);
+}
+
+//this function is what is called when the button is clicked, begins a series of prompts, equipped with validation to make sure their options fit the criterea
+function beginValidation() {
   var passLength = parseInt(prompt("How many characters would you like your password to be?"))
   if (isNaN(passLength)) {
     alert("You must enter a number.")
-    writePassword();
+    return;
   }
   else if (passLength < 8) {
     alert("Passwords must be at least 8 characters long.")
-    writePassword();
+    return;
   }
   else if (passLength > 128) {
     alert("Passwords cannot be longer than 128 characters.")
-    writePassword();
+    return;
   }
-
+  
   var passUp = confirm("Would you like upper case letters in your password?");
   var passLow = confirm("Would you like lower case letters in your password?");
   var passNum = confirm("Would you like numbers in your password?");
@@ -123,9 +181,6 @@ function writePassword() {
   console.log(passLow)
   console.log(passNum)
   console.log(passSpec)
-
-  // this array populates with the true confirm statements and while be compared to later
-  var passOption = []
 
   if (passUp) {
     passOption.push("up");
@@ -143,67 +198,15 @@ function writePassword() {
     passOption.push("spec");
   }
 
-  //final validation
+  //final validation making sure they select at least one char type
   if (passOption == 0) {
     alert("You must select at least 1 character type, try again.")
     return;
   }
-
-  //this is the mathmatical function that creates the random string of characters that will be the password
-  function generatePassword() {
-    var password = "";
-
-    //this loop runs through the array we created above, conisting of place holders that denote which chartype array we will perform random math on
-    for (var i = 0; i < passLength; i++) {
-      var x = passOption[Math.floor(Math.random() * passOption.length)];
-      //these are 4 seperate functions, one for each character type, as they are fired they return their value to concatenate to password
-      function lowerCased() {
-        var y = lowerCasedChar[Math.floor(Math.random() * lowerCasedChar.length)];
-        return password += y;
-      }
-
-      function upperCased() {
-        var y = upperCasedChar[Math.floor(Math.random() * upperCasedChar.length)];
-        return password += y;
-      }
-
-      function number() {
-        var y = numberChar[Math.floor(Math.random() * numberChar.length)];
-        return password += y;
-      }
-
-      function special() {
-        var y = specialChar[Math.floor(Math.random() * specialChar.length)];
-        return password += y;
-      }
-      //this set of conidtions is what the loop is acting on, which ever string in the array is iterated, it will compare to match, and fire the appropriate of the 4 functions
-      if (x == "up") {
-        upperCased();
-        console.log(password)
-      }
-
-      if (x == "low") {
-        lowerCased();
-        console.log(password)
-      }
-
-      if (x == "num") {
-        number();
-        console.log(password)
-      }
-
-      if (x == "spec") {
-        special();
-        console.log(password)
-      }
-    }
-    return password
-  }
-
-  //this takes the return of generate password and appends it to the page
-  document.getElementById("password").innerHTML = generatePassword(password);
+  
+  generatePassword(passLength);
 }
 
-//these event listerners fire the function and remove the readonly installed in the index
-generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", document.getElementById('password').removeAttribute('readonly'));
+
+
+
